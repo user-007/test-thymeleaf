cd8d0ce52d56137ec23212162f27bf8345b3729c
user-007 <wedncv777@gmail.com>
2021-03-27 17:21:31 +0200

    added functionality in the getAllProducts() method

--- src/main/java/thymeleaf/test/testthymeleaf/services/ProductServiceImpl.java
+++ src/main/java/thymeleaf/test/testthymeleaf/services/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package thymeleaf.test.testthymeleaf.services;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import thymeleaf.test.testthymeleaf.model.entities.Product;
 import thymeleaf.test.testthymeleaf.model.entities.User;
@@ -9,15 +10,18 @@ import thymeleaf.test.testthymeleaf.repositories.UserRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
     private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
-    public ProductServiceImpl(ProductRepository productRepository, UserRepository userRepository) {
+    public ProductServiceImpl(ProductRepository productRepository, UserRepository userRepository, ModelMapper modelMapper2) {
         this.productRepository = productRepository;
         this.userRepository = userRepository;
+        this.modelMapper  = modelMapper2;
     }
 
     @Override
@@ -25,12 +29,27 @@ public class ProductServiceImpl implements ProductService {
      List<ProductViewModel> products  = new ArrayList<>();
      List<Product> products_list  = productRepository.findAll();
     products_list.forEach(
+            //returning all products of the given user ;
             el->{
-                User user  = el.getUsername();
-
+                User user  = el.getUser();
+                Optional<ProductViewModel> productViewModel  = findByName(products, user.getUsername());
+                if(!productViewModel.isPresent()){
+                    // not in the result then creating a new model
+                    ProductViewModel newProductViewModel  = new ProductViewModel();
+                    modelMapper.map(user, new ProductViewModel());
+                    products.add(newProductViewModel);
+                    //assigning the new object
+                    productViewModel = Optional.of(newProductViewModel);
+                }
             }
     );
 
 
+    }
+    private static Optional<ProductViewModel> findByName(List<ProductViewModel> allModels, String name){
+        return allModels.
+                stream().
+                filter(m-> m.getName().equals(name)).
+                findAny();
     }
 }
